9e569ec5a2c05a88a884be3ce979d3244be72803
arter97 <qkrwngud825@gmail.com>
2015-07-16 19:21:42 +0900

    fs: drop nodiratime
    noatime implies nodiratime
    
    Change-Id: I4d42555090af7894431ccd8786d7161e186d9ace
    Signed-off-by: arter97 <qkrwngud825@gmail.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2676,10 +2676,9 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 	if (retval)
 		goto dput_out;
 
-	/* Default to noatime/nodiratime unless overriden */
+	/* Default to noatime unless overriden */
 	if (!(flags & MS_RELATIME)) {
 		mnt_flags |= MNT_NOATIME;
-		mnt_flags |= MNT_NODIRATIME;
 	}
 
 	/* Separate the per-mountpoint flags */
