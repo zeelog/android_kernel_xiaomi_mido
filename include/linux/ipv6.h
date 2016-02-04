f711e6613507c7cc4db4e3ea49f4d44acc4ef459
Johannes Berg <johannes.berg@intel.com>
2016-02-04 13:31:20 +0100

    ipv6: add option to drop unsolicited neighbor advertisements
    In certain 802.11 wireless deployments, there will be NA proxies
    that use knowledge of the network to correctly answer requests.
    To prevent unsolicitd advertisements on the shared medium from
    being a problem, on such deployments wireless needs to drop them.
    
    Enable this by providing an option called "drop_unsolicited_na".
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit aec215e7aa380fe5f85eb6948766b58bf78cb6c3)
    
    Change-Id: Iad429a767a786087b0985632be44932b2e3fd1a8
    Signed-off-by: Albert I <kras@raphielgang.org>

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -56,6 +56,7 @@ struct ipv6_devconf {
 	__s32		force_tllao;
 	__s32           ndisc_notify;
 	__s32		suppress_frag_ndisc;
+	__s32		drop_unsolicited_na;
 	__s32		accept_ra_prefix_route;
 	__s32		use_oif_addrs_only;
 	__s32		accept_ra_mtu;
