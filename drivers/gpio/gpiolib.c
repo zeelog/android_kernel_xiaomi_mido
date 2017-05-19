2d0cf842217fdd26f7af285a175ea6f84059750e
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-05-19 16:44:44 +0800

    drivers: input: touchscreen: Add ft5346, ist3038c and gt9xx mido drivers
    * unused firmwares dropped for all
    * test code for ft5346 droped
    * added code to qpnp-smbcharger to notify touchscreen about charger
      connection
    
    [kras: Updated for ten mido bringup; code cleanup]
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -63,8 +63,18 @@ static inline void desc_set_label(struct gpio_desc *d, const char *label)
 /**
  * Convert a GPIO number to its descriptor
  */
+#if defined(CONFIG_MACH_XIAOMI_C6) && defined(CONFIG_TOUCHSCREEN_GT9XX)
+static int special_irq;
+#endif
 struct gpio_desc *gpio_to_desc(unsigned gpio)
 {
+#if defined(CONFIG_MACH_XIAOMI_C6) && defined(CONFIG_TOUCHSCREEN_GT9XX)
+	if (gpio == 65)
+		special_irq = 1;
+	else
+		special_irq = 0;
+#endif
+
 	if (WARN(!gpio_is_valid(gpio), "invalid GPIO %d\n", gpio))
 		return NULL;
 	else
@@ -992,18 +1002,30 @@ int gpiod_direction_input(struct gpio_desc *desc)
 }
 EXPORT_SYMBOL_GPL(gpiod_direction_input);
 
+#if defined(CONFIG_MACH_XIAOMI_C6) && defined(CONFIG_TOUCHSCREEN_GT9XX)
+extern int gt9xx_flag;
+#endif
+
 static int _gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 {
 	struct gpio_chip	*chip;
 	int			status = -EINVAL;
 
 	/* GPIOs used for IRQs shall not be set as output */
-	if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
-		gpiod_err(desc,
-			  "%s: tried to set a GPIO tied to an IRQ as output\n",
-			  __func__);
-		return -EIO;
+#if defined(CONFIG_MACH_XIAOMI_C6) && defined(CONFIG_TOUCHSCREEN_GT9XX)
+	if (special_irq && gt9xx_flag)
+		pr_debug("[GPIO]set GPIO_65 as irq output\n");
+	else {
+#endif
+		if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
+			gpiod_err(desc,
+				 "%s: tried to set a GPIO tied to an IRQ as output\n",
+				 __func__);
+			return -EIO;
+		}
+#if defined(CONFIG_MACH_XIAOMI_C6) && defined(CONFIG_TOUCHSCREEN_GT9XX)
 	}
+#endif
 
 	/* Open drain pin should not be driven to 1 */
 	if (value && test_bit(FLAG_OPEN_DRAIN,  &desc->flags))
