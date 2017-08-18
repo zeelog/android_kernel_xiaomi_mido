5a0038982d8604b6edac6e65f5f21783a8453dba
Joel Fernandes <joelaf@google.com>
2017-08-18 13:29:46 -0700

    sched/fair: Fix issue where frequency update not skipped
    This patch fixes one of the infrequent conditions in
    commit 54b6bae ("sched/fair: Skip frequency updates if CPU about to idle")
    where we could have skipped a frequency update. The fix is to use the
    correct flag which skips freq updates.
    
    Note that this is a rare issue (can show up only during CFS throttling)
    and even then we just do an additional frequency update which we were
    doing anyway before the above patch.
    
    Bug: 64689959
    
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: I0aef9d7b70b190325412f9aba285997c40b51177
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3351,7 +3351,7 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
  */
 #define UPDATE_TG	0x1
 #define SKIP_AGE_LOAD	0x2
-#define SKIP_CPUFREQ	0x4
+#define SKIP_CPUFREQ	0x0
 
 /* Update task and its cfs_rq load average */
 static inline void update_load_avg(struct sched_entity *se, int flags)
@@ -5097,6 +5097,8 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	}
 
 	for_each_sched_entity(se) {
+		int update_flags;
+
 		cfs_rq = cfs_rq_of(se);
 		cfs_rq->h_nr_running--;
 		walt_dec_cfs_rq_stats(cfs_rq, p);
@@ -5104,7 +5106,12 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		if (cfs_rq_throttled(cfs_rq))
 			break;
 
-		update_load_avg(se, UPDATE_TG | (flags & DEQUEUE_IDLE));
+		update_flags = UPDATE_TG;
+
+		if (flags & DEQUEUE_IDLE)
+			update_flags |= SKIP_CPUFREQ;
+
+		update_load_avg(se, update_flags);
 		update_cfs_shares(se);
 	}
 
