a2b83d1fd948f39efdba9ce7cddea1f4b189ee7e
Peter Zijlstra <peterz@infradead.org>
2017-08-29 15:07:54 +0200

    sched/idle: Move quiet_vmstate() into the NOHZ code
    quiet_vmstat() is an expensive function that only makes sense when we
    go into NOHZ.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: aubrey.li@linux.intel.com
    Cc: cl@linux.com
    Cc: fweisbec@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -25,6 +25,7 @@
 #include <linux/timer.h>
 #include <linux/context_tracking.h>
 #include <linux/rq_stats.h>
+#include <linux/mm.h>
 
 #include <asm/irq_regs.h>
 
@@ -822,6 +823,7 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 		nohz_balance_enter_idle(cpu);
 		calc_load_enter_idle();
 		cpu_load_update_nohz_start();
+		quiet_vmstat();
 
 		ts->last_tick = hrtimer_get_expires(&ts->sched_timer);
 		ts->tick_stopped = 1;
