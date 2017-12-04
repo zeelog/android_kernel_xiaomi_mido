769c20b1996af42c7da6c1cb73984e2d2950eeb6
Albert I <krascgq@outlook.co.id>
2017-12-04 20:00:31 +0800

    yama: Default to NO_ATTACH
    Change-Id: I96ef89d9be0a7293c2b3a30667013a91823cdb77
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- security/yama/yama_lsm.c
+++ security/yama/yama_lsm.c
@@ -24,7 +24,7 @@
 #define YAMA_SCOPE_CAPABILITY	2
 #define YAMA_SCOPE_NO_ATTACH	3
 
-static int ptrace_scope = YAMA_SCOPE_RELATIONAL;
+static int ptrace_scope = YAMA_SCOPE_NO_ATTACH;
 
 /* describe a ptrace relationship for potential exception */
 struct ptrace_relation {
