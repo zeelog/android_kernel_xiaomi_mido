9943aafb5675fc935f62767e6f0b26922e1e20c0
Albert I <krascgq@outlook.co.id>
2017-12-05 17:30:15 +0800

    dcache: Set VFS cache pressure to 75
    Change-Id: Ib9e48f8c0f5ba6cba7052654ba888049291acc10
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -479,7 +479,7 @@ extern int sysctl_vfs_cache_pressure;
 
 static inline unsigned long vfs_pressure_ratio(unsigned long val)
 {
-	return mult_frac(val, sysctl_vfs_cache_pressure, 100);
+	return mult_frac(val, sysctl_vfs_cache_pressure, 75);
 }
 
 /**
