4006f99258480d5894d8dd48f5b7eacbb42c1671
Sami Tolvanen <samitolvanen@google.com>
2017-12-08 15:49:24 -0800

    ANDROID: init: ensure initcall ordering with LTO
    With LTO, the compiler doesn't necessarily obey link order for
    initcalls, and the initcall variable needs to be globally unique
    to avoid naming collisions.
    
    In order to preserve the correct order, we add each variable
    into its own section and generate a linker script (in
    scripts/link-vmlinux.sh) to ensure the order remains correct.  We
    also add a __COUNTER__ prefix to the name, so we can retain the
    order of initcalls within each compilation unit, and __LINE__ to
    make the names more unique.
    
    Bug: 145210207
    Change-Id: Iddda881a52b7942781713b188d810b6100159a2b
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -62,29 +62,30 @@ archive_builtin()
 	fi
 }
 
-# If CONFIG_LTO_CLANG is selected, collect generated symbol versions into
-# .tmp_symversions
-modversions()
+# If CONFIG_LTO_CLANG is selected, generate a linker script to ensure correct
+# ordering of initcalls, and with CONFIG_MODVERSIONS also enabled, collect the
+# previously generated symbol versions into the same script.
+lto_lds()
 {
 	if [ -z "${CONFIG_LTO_CLANG}" ]; then
 		return
 	fi
 
-	if [ -z "${CONFIG_MODVERSIONS}" ]; then
-		return
+	${srctree}/scripts/generate_initcall_order.pl \
+		built-in.o ${KBUILD_VMLINUX_LIBS} \
+		> .tmp_lto.lds
+
+	if [ -n "${CONFIG_MODVERSIONS}" ]; then
+		for a in built-in.o ${KBUILD_VMLINUX_LIBS}; do
+			for o in $(${AR} t $a); do
+				if [ -f ${o}.symversions ]; then
+					cat ${o}.symversions >> .tmp_lto.lds
+				fi
+			done
+		done
 	fi
 
-	rm -f .tmp_symversions
-
-	for a in built-in.o ${KBUILD_VMLINUX_LIBS}; do
-		for o in $(${AR} t $a); do
-			if [ -f ${o}.symversions ]; then
-				cat ${o}.symversions >> .tmp_symversions
-			fi
-		done
-	done
-
-	echo "-T .tmp_symversions"
+	echo "-T .tmp_lto.lds"
 }
 
 # Link of vmlinux.o used for section mismatch analysis
@@ -110,7 +111,7 @@ modpost_link()
 		info LD vmlinux.o
 	fi
 
-	${LD} ${LDFLAGS} -r -o ${1} $(modversions) ${objects}
+	${LD} ${LDFLAGS} -r -o ${1} $(lto_lds) ${objects}
 }
 
 # If CONFIG_LTO_CLANG is selected, we postpone running recordmcount until
@@ -248,7 +249,7 @@ cleanup()
 	rm -f .tmp_System.map
 	rm -f .tmp_kallsyms*
 	rm -f .tmp_version
-	rm -f .tmp_symversions
+	rm -f .tmp_lto.lds
 	rm -f .tmp_vmlinux*
 	rm -f built-in.o
 	rm -f System.map
