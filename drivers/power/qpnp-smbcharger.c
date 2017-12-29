8d1d442e377bf28f7e4587615176978e2fb92d73
TheScarastic <warabhishek@gmail.com>
2017-12-29 20:01:41 +0530

    drivers: power: Import mido changes
    [kras: Updated for ten mido bringup; code cleanup, fix slight derp]
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -4997,6 +4997,9 @@ static bool is_usbin_uv_high(struct smbchg_chip *chip)
 }
 
 #define HVDCP_NOTIFY_MS		2500
+#ifdef CONFIG_MACH_XIAOMI_C6
+static int rerun_apsd(struct smbchg_chip *chip);
+#endif
 static void handle_usb_insertion(struct smbchg_chip *chip)
 {
 	enum power_supply_type usb_supply_type;
@@ -5015,6 +5018,12 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 	pr_smb(PR_STATUS, "triggered\n");
 	/* usb inserted */
 	read_usb_type(chip, &usb_type_name, &usb_supply_type);
+#ifdef CONFIG_MACH_XIAOMI_C6
+	if (usb_supply_type == POWER_SUPPLY_TYPE_USB_CDP) {
+		rc = rerun_apsd(chip);
+		read_usb_type(chip, &usb_type_name, &usb_supply_type);
+	}
+#endif
 	pr_smb(PR_STATUS,
 		"inserted type = %d (%s)", usb_supply_type, usb_type_name);
 
@@ -6109,6 +6118,7 @@ static void smbchg_external_power_changed(struct power_supply *psy)
 
 	read_usb_type(chip, &usb_type_name, &usb_supply_type);
 
+#ifndef CONFIG_MACH_XIAOMI_C6
 	if (!rc && usb_supply_type == POWER_SUPPLY_TYPE_USB &&
 			prop.intval != POWER_SUPPLY_TYPE_USB &&
 			is_usb_present(chip)) {
@@ -6148,6 +6158,7 @@ static void smbchg_external_power_changed(struct power_supply *psy)
 
 	if (usb_supply_type != POWER_SUPPLY_TYPE_USB)
 		goto  skip_current_for_non_sdp;
+#endif
 
 	pr_smb(PR_MISC, "usb type = %s current_limit = %d\n",
 			usb_type_name, current_limit);
@@ -6157,7 +6168,9 @@ static void smbchg_external_power_changed(struct power_supply *psy)
 	if (rc < 0)
 		pr_err("Couldn't update USB PSY ICL vote rc=%d\n", rc);
 
+#ifndef CONFIG_MACH_XIAOMI_C6
 skip_current_for_non_sdp:
+#endif
 	smbchg_vfloat_adjust_check(chip);
 
 	power_supply_changed(&chip->batt_psy);
@@ -6350,7 +6363,11 @@ static int smbchg_battery_get_property(struct power_supply *psy,
 		val->intval = get_prop_batt_health(chip);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
+#ifdef CONFIG_MACH_XIAOMI_C6
+		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
+#else
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+#endif
 		break;
 	case POWER_SUPPLY_PROP_FLASH_CURRENT_MAX:
 		val->intval = smbchg_calc_max_flash_current(chip);
@@ -6563,6 +6580,23 @@ static irqreturn_t batt_warm_handler(int irq, void *_chip)
 {
 	struct smbchg_chip *chip = _chip;
 	u8 reg = 0;
+#ifdef CONFIG_MACH_XIAOMI_C6
+	int rc;
+
+	/* set the warm float voltage compensation,
+	 * set the warm float voltage to 4.1V
+	 */
+	if (chip->float_voltage_comp != -EINVAL) {
+		rc = smbchg_float_voltage_comp_set(chip,
+						chip->float_voltage_comp);
+		if (rc < 0)
+			dev_err(chip->dev,
+				"Couldn't set float voltage comp rc = %d\n",
+				rc);
+		pr_smb(PR_STATUS, "set float voltage comp to %d\n",
+			chip->float_voltage_comp);
+	}
+#endif
 
 	smbchg_read(chip, &reg, chip->bat_if_base + RT_STS, 1);
 	chip->batt_warm = !!(reg & HOT_BAT_SOFT_BIT);
@@ -6579,6 +6613,17 @@ static irqreturn_t batt_cool_handler(int irq, void *_chip)
 {
 	struct smbchg_chip *chip = _chip;
 	u8 reg = 0;
+#ifdef CONFIG_MACH_XIAOMI_C6
+	int rc;
+
+	/* set the cool float voltage compensation,
+	 * set the cool float voltage to 4.4V
+	 */
+	rc = smbchg_float_voltage_comp_set(chip, 0);
+	if (rc < 0)
+		dev_err(chip->dev, "Couldn't set float voltage comp rc = %d\n",
+			rc);
+#endif
 
 	smbchg_read(chip, &reg, chip->bat_if_base + RT_STS, 1);
 	chip->batt_cool = !!(reg & COLD_BAT_SOFT_BIT);
@@ -7644,6 +7689,23 @@ static int smbchg_hw_init(struct smbchg_chip *chip)
 		if (rc < 0)
 			dev_err(chip->dev, "Couldn't set OTG OC config rc = %d\n",
 				rc);
+
+#ifdef CONFIG_MACH_XIAOMI_C6
+		rc = smbchg_sec_masked_write(chip, chip->otg_base + OTG_CFG,
+					     0x0c, 0x8);
+		if (rc < 0) {
+			dev_err(chip->dev,
+				"Couldn't set SMBCHGL_OTG_CFG rc=%d\n", rc);
+		}
+
+		rc = smbchg_read(chip, &reg, chip->otg_base + OTG_CFG, 1);
+		pr_debug("%s:read OTG_CFG=%2x\n", __func__, reg);
+		if (rc < 0) {
+			dev_err(chip->dev,
+				"Couldn't set SMBCHGL_OTG_CFG rc=%d\n", rc);
+		}
+#endif
+
 	}
 
 	if (chip->otg_pinctrl) {
@@ -8633,6 +8695,10 @@ static int smbchg_probe(struct spmi_device *spmi)
 		dev_err(chip->dev, "Error parsing DT peripherals: %d\n", rc);
 		goto votables_cleanup;
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_C6
+	chip->hvdcp_not_supported = true;
+#endif
 
 	rc = smbchg_check_chg_version(chip);
 	if (rc) {
