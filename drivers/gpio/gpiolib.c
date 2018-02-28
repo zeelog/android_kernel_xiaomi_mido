0c63139e3a7ea9dfd1088f639db9a8853823d242
Albert I <krascgq@outlook.co.id>
2018-02-28 20:11:34 +0800

    drivers: input: touchscreen: Build correct gt9xx drivers
    So we don't accidentally enable both versions of the same driver.
    
    Change-Id: I25376b4a18d701198bf82621fe7cef185c2fd39d
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -63,12 +63,12 @@ static inline void desc_set_label(struct gpio_desc *d, const char *label)
 /**
  * Convert a GPIO number to its descriptor
  */
-#ifdef CONFIG_TOUCHSCREEN_GT9XX_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) && defined(CONFIG_TOUCHSCREEN_GT9XX)
 static int special_irq;
 #endif
 struct gpio_desc *gpio_to_desc(unsigned gpio)
 {
-#ifdef CONFIG_TOUCHSCREEN_GT9XX_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) && defined(CONFIG_TOUCHSCREEN_GT9XX)
 	if (gpio == 65)
 		special_irq = 1;
 	else
@@ -993,7 +993,7 @@ int gpiod_direction_input(struct gpio_desc *desc)
 }
 EXPORT_SYMBOL_GPL(gpiod_direction_input);
 
-#ifdef CONFIG_TOUCHSCREEN_GT9XX_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) && defined(CONFIG_TOUCHSCREEN_GT9XX)
 extern int gt9xx_flag;
 #endif
 
@@ -1003,7 +1003,7 @@ static int _gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 	int			status = -EINVAL;
 
 	/* GPIOs used for IRQs shall not be set as output */
-#ifdef CONFIG_TOUCHSCREEN_GT9XX_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) && defined(CONFIG_TOUCHSCREEN_GT9XX)
 	if ((special_irq == 1) && (gt9xx_flag == 1)) {
 		printk("[GPIO]set GPIO_65 as irq output\n");
 	} else{
@@ -1014,7 +1014,7 @@ static int _gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 					__func__);
 			return -EIO;
 		}
-#ifdef CONFIG_TOUCHSCREEN_GT9XX_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) && defined(CONFIG_TOUCHSCREEN_GT9XX)
 	}
 #endif
 
