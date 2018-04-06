626f397d4b1e3727ace7a412d690114714b59264
Francisco Franco <franciscofranco.1990@gmail.com>
2018-04-06 15:37:41 +0000

    cpufreq: cpu-boost: set default cpuboost values
    Just in case our A/B flashing (which requires root to apply ramdisk edits)
    fucks up, at least we make sure the device is smooth enough when
    interacting with it.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: khusika <khusikadhamar@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -35,7 +35,7 @@ static DEFINE_PER_CPU(struct cpu_sync, sync_info);
 static unsigned int input_boost_enabled = 1;
 module_param(input_boost_enabled, uint, 0644);
 
-static unsigned int input_boost_ms = 40;
+static unsigned int input_boost_ms = 1500;
 module_param(input_boost_ms, uint, 0644);
 
 static bool sched_boost_on_input;
@@ -344,6 +344,7 @@ static int cpu_boost_init(void)
 	for_each_possible_cpu(cpu) {
 		s = &per_cpu(sync_info, cpu);
 		s->cpu = cpu;
+		s->input_boost_freq = 1036800;
 	}
 	cpufreq_register_notifier(&boost_adjust_nb, CPUFREQ_POLICY_NOTIFIER);
 	ret = input_register_handler(&cpuboost_input_handler);
