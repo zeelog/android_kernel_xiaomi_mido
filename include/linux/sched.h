c3ffe243bacfccaec7bc605513c0bea6161c8303
Rick Yiu <rickyiu@google.com>
2018-05-18 11:54:10 +0800

    sched/fair: remove sync logic from select_energy_cpu_brute
    This patch is originated from wahoo-4.4 kernel,
    commit 9767a33691cb ("Move sync logic out of select_energy_cpu_brute")
    but only part of it, since sync logic is already under
    select_task_rq_fair().
    
    bug: 78790904
    Change-Id: I22e27cc3b6218e4eff10847af4d9cce93a99cf38
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    
     Conflicts:
    	include/trace/events/sched.h
    	kernel/sched/fair.c
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1173,7 +1173,6 @@ struct eas_stats {
 
 	/* select_energy_cpu_brute() stats */
 	u64 secb_attempts;
-	u64 secb_sync;
 	u64 secb_idle_bt;
 	u64 secb_insuff_cap;
 	u64 secb_no_nrg_sav;
@@ -1472,7 +1471,6 @@ struct sched_statistics {
 
 	/* energy_aware_wake_cpu() */
 	u64			nr_wakeups_secb_attempts;
-	u64			nr_wakeups_secb_sync;
 	u64			nr_wakeups_secb_idle_bt;
 	u64			nr_wakeups_secb_insuff_cap;
 	u64			nr_wakeups_secb_no_nrg_sav;
