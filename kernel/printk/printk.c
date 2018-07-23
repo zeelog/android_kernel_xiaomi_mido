fca83156c0fbfe2fb50c4878959a363d63d2a7e3
kdrag0n <dragon@khronodragon.com>
2018-07-23 15:05:12 -0700

    printk: Silence healthd messages from userspace
    Constant messages about battery status every second doesn't help battery
    or debugging.
    
    [celtare21: Fix the memory leak]
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    [Lau: Fix identation]
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -769,6 +769,11 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 		}
 	}
 
+	if (strncmp("healthd", line, 7) == 0) {
+		kfree(buf);
+		return len;
+	}
+
 	printk_emit(facility, level, NULL, 0, "%s", line);
 	kfree(buf);
 	return ret;
