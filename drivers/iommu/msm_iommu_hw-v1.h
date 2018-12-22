f88b0990e9102ca222ff196ad191f814c468980d
Alberto97 <albertop2197@gmail.com>
2018-12-22 17:45:19 +0100

    drivers: iommu: Fix shift overflow
    drivers/iommu/msm_iommu-v1.c:880:2: error: signed shift result (0x3FFFFFC000) requires 39 bits to represent, but 'int' only has 32 bits [-Werror,-Wshift-overflow]
            SET_CB_TTBR0_ADDR(cb_base, ctx, pn);
            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/iommu/msm_iommu_hw-v1.h:1025:37: note: expanded from macro 'SET_CB_TTBR0_ADDR'
                                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Alberto97 <albertop2197@gmail.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/iommu/msm_iommu_hw-v1.h
+++ drivers/iommu/msm_iommu_hw-v1.h
@@ -1018,7 +1018,7 @@ do { \
 #define SET_CB_TTBR0_RGN(b, c, v)   SET_CONTEXT_FIELD(b, c, CB_TTBR0, RGN, v)
 #define SET_CB_TTBR0_NOS(b, c, v)   SET_CONTEXT_FIELD(b, c, CB_TTBR0, NOS, v)
 #define SET_CB_TTBR0_IRGN0(b, c, v) SET_CONTEXT_FIELD(b, c, CB_TTBR0, IRGN0, v)
-#define SET_CB_TTBR0_ADDR(b, c, v)  SET_CONTEXT_FIELD(b, c, CB_TTBR0, ADDR, v)
+#define SET_CB_TTBR0_ADDR(b, c, v)  SET_CONTEXT_FIELD_Q(b, c, CB_TTBR0, ADDR, v)
 
 #define GET_CB_TTBR0_IRGN1(b, c)    GET_CONTEXT_FIELD(b, c, CB_TTBR0, IRGN1)
 #define GET_CB_TTBR0_S(b, c)        GET_CONTEXT_FIELD(b, c, CB_TTBR0, S)
