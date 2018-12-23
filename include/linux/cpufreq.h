2fb0abfd0a45f4ba1b182b7a5a492d69f15f010a
Michael Bestas <mkbestas@lineageos.org>
2018-12-23 14:23:22 +0200

    ANDROID: cpufreq: stats: Fix ARM compilation
    Commit 49e35712db1f0 ANDROID: cpufreq_stat: add per task/uid/freq stats
    broke ARM compilation because of the different header inclusion chain.
    Fixing it properly would require making a lot of changes in the header
    inclusion chain so simply define the struct before usage to fix the issue.
    
    This fixes compilation for the following ARM configs:
    msm8937-perf_defconfig
    msm8937_defconfig
    msmcortex-perf_defconfig
    msmcortex_defconfig
    
    Change-Id: I27fa83b5eb7a11e7607ed922f63fe4235e39bf5e
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -654,6 +654,8 @@ void acct_update_power(struct task_struct *p, cputime_t cputime);
 void cpufreq_task_stats_init(struct task_struct *p);
 void cpufreq_task_stats_exit(struct task_struct *p);
 void cpufreq_task_stats_remove_uids(uid_t uid_start, uid_t uid_end);
+
+struct pid_namespace;
 int  proc_time_in_state_show(struct seq_file *m, struct pid_namespace *ns,
 	struct pid *pid, struct task_struct *p);
 
