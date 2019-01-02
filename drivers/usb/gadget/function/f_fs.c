7cc69e90ea5e1208e1b0eef088d910903d29c9b8
Albert I <krascgq@outlook.co.id>
2019-01-02 23:44:20 +0800

    treewide: Fix compilation warnings detected by Linaro GCC 7.3.1
    * Using latest 2018.05 prebuilt release from releases.linaro.org.
    * Tested against mido_defconfig and tissot_defconfig.
    * Patch is for Pie kernel, contains fixes kanged from various sources.
    
    Change-Id: I5421ac64332857c31fd587e412b97e98ac623c8a
    Co-authored-by: Nathan Chancellor <natechancellor@gmail.com>
    Co-authored-by: Adek Maulana <adekzmaulana@gmail.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -1716,7 +1716,8 @@ static void functionfs_unbind(struct ffs_data *ffs)
 static int ffs_epfiles_create(struct ffs_data *ffs)
 {
 	struct ffs_epfile *epfile, *epfiles;
-	unsigned i, count;
+	short i;
+	unsigned count;
 
 	ENTER();
 
