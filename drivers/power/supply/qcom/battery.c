734cbe33e8d83187644c3f076160e5bf4cc69433
Michael <loukerismichalis@gmail.com>
2019-02-07 14:15:48 +0200

    power:supply:qcom: queue works into a power efficient WQ

--- drivers/power/supply/qcom/battery.c
+++ drivers/power/supply/qcom/battery.c
@@ -810,7 +810,8 @@ stepper_exit:
 	chip->slave_fcc_ua = parallel_fcc;
 
 	if (reschedule_ms) {
-		schedule_delayed_work(&chip->fcc_stepper_work,
+		queue_delayed_work(system_power_efficient_wq,
+			&chip->fcc_stepper_work,
 				msecs_to_jiffies(reschedule_ms));
 		pr_debug("Rescheduling FCC_STEPPER work\n");
 		return;
@@ -927,8 +928,9 @@ static int usb_icl_vote_callback(struct votable *votable, void *data,
 	if (icl_ua <= 1400000)
 		vote(chip->pl_enable_votable_indirect, USBIN_I_VOTER, false, 0);
 	else
-		schedule_delayed_work(&chip->status_change_work,
-						msecs_to_jiffies(PL_DELAY_MS));
+		queue_delayed_work(system_power_efficient_wq,
+			&chip->status_change_work,
+				msecs_to_jiffies(PL_DELAY_MS));
 
 	/* rerun AICL */
 	/* get the settled current */
@@ -1071,8 +1073,8 @@ static int pl_disable_vote_callback(struct votable *votable,
 			if (chip->step_fcc) {
 				vote(chip->pl_awake_votable, FCC_STEPPER_VOTER,
 					true, 0);
-				schedule_delayed_work(&chip->fcc_stepper_work,
-					0);
+				queue_delayed_work(system_power_efficient_wq,
+					&chip->fcc_stepper_work,0);
 				}
 		} else {
 			/*
@@ -1203,16 +1205,17 @@ static int pl_disable_vote_callback(struct votable *votable,
 			if (chip->step_fcc) {
 				vote(chip->pl_awake_votable, FCC_STEPPER_VOTER,
 					true, 0);
-				schedule_delayed_work(&chip->fcc_stepper_work,
-					0);
+				queue_delayed_work(system_power_efficient_wq,
+					&chip->fcc_stepper_work,0);
 			}
 		}
 
 		rerun_election(chip->fv_votable);
 
 		cancel_delayed_work_sync(&chip->pl_awake_work);
-		schedule_delayed_work(&chip->pl_awake_work,
-						msecs_to_jiffies(5000));
+		queue_delayed_work(system_power_efficient_wq,
+			&chip->pl_awake_work,
+				msecs_to_jiffies(5000));
 	}
 
 	/* notify parallel state change */
@@ -1544,7 +1547,8 @@ static int pl_notifier_call(struct notifier_block *nb,
 	if ((strcmp(psy->desc->name, "parallel") == 0)
 	    || (strcmp(psy->desc->name, "battery") == 0)
 	    || (strcmp(psy->desc->name, "main") == 0))
-		schedule_delayed_work(&chip->status_change_work, 0);
+		queue_delayed_work(system_power_efficient_wq,
+			&chip->status_change_work, 0);
 
 	return NOTIFY_OK;
 }
