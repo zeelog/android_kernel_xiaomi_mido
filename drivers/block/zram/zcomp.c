16232c811f014e082e9a3fc929e2e6b1de45890c
Dave Rodgman <dave.rodgman@arm.com>
2019-03-07 16:30:44 -0800

    lib/lzo: separate lzo-rle from lzo
    To prevent any issues with persistent data, separate lzo-rle from lzo so
    that it is treated as a separate algorithm, and lzo is still available.
    
    Link: http://lkml.kernel.org/r/20190205155944.16007-3-dave.rodgman@arm.com
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: Markus F.X.J. Oberhumer <markus@oberhumer.com>
    Cc: Matt Sealey <matt.sealey@arm.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <nitingupta910@gmail.com>
    Cc: Richard Purdie <rpurdie@openedhand.com>
    Cc: Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
    Cc: Sonny Rao <sonnyrao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -19,8 +19,11 @@
 #include "zcomp.h"
 
 static const char * const backends[] = {
+	"lzo",
+	"lzo-rle",
+#if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 	"lz4",
-#if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
+#else if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
 	"zstd",
 #endif
 	NULL
