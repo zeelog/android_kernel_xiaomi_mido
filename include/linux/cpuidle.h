d21cfd2b2db0794077171bf2e95cae059fe34392
kdrag0n <dragon@khronodragon.com>
2019-03-09 18:52:04 -0800

    cpuidle: Add cpuidle_use_deepest_state_mask() helper function
    The cpuidle_use_deepest_state() function introduced in commit
    2847a3ac96d32 only affects the calling CPU. Add a helper function that
    affects all CPUs in a given cpumask to make it easier to toggle
    use_deepest_state for multiple CPUs at once and reduce boilerplate code
    wherever such behavior is desired.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -199,6 +199,8 @@ extern int cpuidle_find_deepest_state(struct cpuidle_driver *drv,
 extern int cpuidle_enter_freeze(struct cpuidle_driver *drv,
 				struct cpuidle_device *dev);
 extern void cpuidle_use_deepest_state(bool enable);
+extern int cpuidle_use_deepest_state_mask(const struct cpumask *target,
+					  bool enable);
 #else
 static inline int cpuidle_find_deepest_state(struct cpuidle_driver *drv,
 					     struct cpuidle_device *dev)
@@ -208,6 +210,11 @@ static inline int cpuidle_enter_freeze(struct cpuidle_driver *drv,
 {return -ENODEV; }
 static inline void cpuidle_use_deepest_state(bool enable)
 {
+}
+static inline int cpuidle_use_deepest_state_mask(const struct cpumask *target,
+						 bool enable)
+{
+	return -ENODEV;
 }
 #endif
 
