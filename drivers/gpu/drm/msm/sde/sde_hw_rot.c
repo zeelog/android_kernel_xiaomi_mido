39e1b6652d6db3e3a6875afd822dafb0d0bac331
Park Ju Hyung <qkrwngud825@gmail.com>
2019-05-06 21:44:05 +0900

    drm/msm: skip sde_dbg compilation altogether
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/gpu/drm/msm/sde/sde_hw_rot.c
+++ drivers/gpu/drm/msm/sde/sde_hw_rot.c
@@ -62,16 +62,6 @@ static struct sde_rot_cfg *_rot_offset(enum sde_rot rot,
 }
 
 /**
- * _sde_hw_rot_reg_dump - perform register dump
- * @ptr: private pointer to rotator platform device
- * return: None
- */
-static void _sde_hw_rot_reg_dump(void *ptr)
-{
-	sde_rotator_inline_reg_dump((struct platform_device *) ptr);
-}
-
-/**
  * sde_hw_rot_start - start rotator before any commit
  * @hw: Pointer to rotator hardware driver
  * return: 0 if success; error code otherwise
@@ -87,10 +77,6 @@ static int sde_hw_rot_start(struct sde_hw_rot *hw)
 	}
 
 	pdev = hw->caps->pdev;
-
-	rc = sde_dbg_reg_register_cb(hw->name, _sde_hw_rot_reg_dump, pdev);
-	if (rc)
-		SDE_ERROR("failed to register debug dump %d\n", rc);
 
 	hw->rot_ctx = sde_rotator_inline_open(pdev);
 	if (IS_ERR_OR_NULL(hw->rot_ctx)) {
@@ -116,9 +102,6 @@ static void sde_hw_rot_stop(struct sde_hw_rot *hw)
 
 	sde_rotator_inline_release(hw->rot_ctx);
 	hw->rot_ctx = NULL;
-
-	sde_dbg_reg_unregister_cb(hw->name, _sde_hw_rot_reg_dump,
-			hw->caps->pdev);
 }
 
 /**
