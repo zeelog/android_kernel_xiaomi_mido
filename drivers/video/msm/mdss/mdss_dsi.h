cf6cc955babb6dc82cd6933e1b494f001abc51f8
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-06 17:47:23 -0700

    msm: mdss: Reduce display commit latency caused by deep CPU idle
    During heavy parts of the display commit process, the CPU can enter a
    deep idle state and cause the commit latency to increase significantly.
    
    Forcing the CPU to only use idle states with latencies under 100 us
    during the critical portions of the commit process fixes the high commit
    latency.
    
    Change-Id: I9e55a626f8273c0ead99c15823c539188b2139bf
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -19,6 +19,7 @@
 #include <linux/irqreturn.h>
 #include <linux/pinctrl/consumer.h>
 #include <linux/gpio.h>
+#include <linux/pm_qos.h>
 
 #include "mdss_panel.h"
 #include "mdss_dsi_cmd.h"
@@ -564,6 +565,15 @@ struct mdss_dsi_ctrl_pdata {
 	bool update_phy_timing; /* flag to recalculate PHY timings */
 
 	bool phy_power_off;
+
+	struct notifier_block wake_notif;
+	struct task_struct *wake_thread;
+	struct completion wake_comp;
+	wait_queue_head_t wake_waitq;
+	atomic_t disp_is_on;
+	atomic_t needs_wake;
+
+	struct pm_qos_request pm_qos_req;
 };
 
 struct dsi_status_data {
