3b40b7e02aad87f813b8746fce2283406e688a87
Albert I <kras@raphielgang.org>
2019-05-16 21:23:10 +0800

    lowmemorykiller: Revert to android-3.18 version at 73abc99b42cf
    CAF's LMK is overly aggressive.
    This also reverts changes that are only necessary for CAF's LMK.
    
    The revert patch is for msm-3.18 Pie kernel.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -796,7 +796,6 @@ const char * const vmstat_text[] = {
 	"workingset_nodereclaim",
 	"nr_anon_transparent_hugepages",
 	"nr_free_cma",
-	"nr_swapcache",
 
 	/* enum writeback_stat_item counters */
 	"nr_dirty_threshold",
