7194364ac5ba39fcce2603d17fb532b76a3afec3
Park Ju Hyung <qkrwngud825@gmail.com>
2019-06-24 21:03:39 +0900

    cpuhotplug: stop IRQ hotplug messages
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- kernel/irq/cpuhotplug.c
+++ kernel/irq/cpuhotplug.c
@@ -106,10 +106,6 @@ void irq_migrate_all_off_this_cpu(void)
 		raw_spin_lock(&desc->lock);
 		affinity_broken = migrate_one_irq(desc);
 		raw_spin_unlock(&desc->lock);
-
-		if (affinity_broken)
-			pr_warn_ratelimited("IRQ%u no longer affine to CPU%u\n",
-					    irq, smp_processor_id());
 	}
 
 	local_irq_restore(flags);
