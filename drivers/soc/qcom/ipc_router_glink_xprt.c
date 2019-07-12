793cdfad1ee4bb90c264ad96222de15cfc7c889a
celtare21 <celtare21@gmail.com>
2019-07-12 21:14:48 +0000

    soc: qcom: glink: Disable debug
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/soc/qcom/ipc_router_glink_xprt.c
+++ drivers/soc/qcom/ipc_router_glink_xprt.c
@@ -10,11 +10,6 @@
  * GNU General Public License for more details.
  */
 
-/*
- * IPC ROUTER GLINK XPRT module.
- */
-#define DEBUG
-
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/types.h>
@@ -28,7 +23,7 @@
 #include <soc/qcom/glink.h>
 #include <soc/qcom/subsystem_restart.h>
 
-static int ipc_router_glink_xprt_debug_mask;
+static int ipc_router_glink_xprt_debug_mask = 0;
 module_param_named(debug_mask, ipc_router_glink_xprt_debug_mask,
 		   int, 0664);
 
