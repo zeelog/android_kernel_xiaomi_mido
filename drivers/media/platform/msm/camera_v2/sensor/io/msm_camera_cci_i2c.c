4ac53924adcd3576fad12d31e01f05d33cb25bd7
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-30 23:38:19 -0700

    msm: camera-v2-legacy: Optimize memory allocation for small buffers
    Try to use an on-stack buffer for memory allocations that are small
    enough to not warrant a dynamic allocation, and eliminate dynamic memory
    allocation entirely in msm_camera_cci_i2c_read_seq. This improves
    performance by skipping latency-prone dynamic memory allocation when it
    isn't needed.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c
@@ -93,6 +93,7 @@ int32_t msm_camera_cci_i2c_read_seq(struct msm_camera_i2c_client *client,
 	CDBG("%s line %d rc = %d\n", __func__, __LINE__, rc);
 	rc = cci_ctrl.status;
 
+
 	S_I2C_DBG("%s addr = 0x%x", __func__, addr);
 	for (i = 0; i < num_byte; i++) {
 		data[i] = buf[i];
@@ -145,6 +146,8 @@ int32_t msm_camera_cci_i2c_write_seq(struct msm_camera_i2c_client *client,
 	uint32_t i = 0;
 	struct msm_camera_cci_ctrl cci_ctrl;
 	struct msm_camera_i2c_reg_array *reg_conf_tbl = NULL;
+	struct msm_camera_i2c_reg_array reg_tbl_onstack[
+		SZ_4K / sizeof(struct msm_camera_i2c_reg_array)];
 
 	if ((client->addr_type != MSM_CAMERA_I2C_BYTE_ADDR
 		&& client->addr_type != MSM_CAMERA_I2C_WORD_ADDR)
