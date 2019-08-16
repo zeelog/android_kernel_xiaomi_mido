a726592cfe3009ec3c8cf9f18622171d2e297da3
Tyler Nijmeh <tylernij@gmail.com>
2019-08-15 23:17:35 -0700

    net: Allow BFP JIT to compile without module support
    Thanks to kdrag0n @ GitHub for his original commit using vmalloc instead
    of kmalloc (preventing a panic).
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Change-Id: I336835a0bf9abbbbad0b9a0d299b5c22eaf15abb
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -312,12 +312,20 @@ static void bpf_jit_uncharge_modmem(u32 pages)
 
 void *__weak bpf_jit_alloc_exec(unsigned long size)
 {
+#ifdef CONFIG_MODULES
 	return module_alloc(size);
+#else
+	return vmalloc_exec(size);
+#endif
 }
 
 void __weak bpf_jit_free_exec(void *addr)
 {
+#ifdef CONFIG_MODULES
 	module_memfree(addr);
+#else
+	vfree(addr);
+#endif
 }
 
 struct bpf_binary_header *
