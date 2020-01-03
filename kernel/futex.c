91190602e9d8267d03530c114648c71dfad24049
Michael Bestas <mkbestas@lineageos.org>
2020-01-03 23:47:57 +0200

    Revert "BACKPORT: futex: Prevent overflow by strengthen input validation"
    * Already applied in ad211e59c6838
    
    This reverts commit 7026d0657625f65d5c40ec937276630cdaf8cdd7.
    
    Change-Id: Ie90789577c5e8ea039b3ccd9afcf48c419f8a386

--- kernel/futex.c
+++ kernel/futex.c
@@ -1619,9 +1619,6 @@ static int futex_requeue(u32 __user *uaddr1, unsigned int flags,
 	if (nr_wake < 0 || nr_requeue < 0)
 		return -EINVAL;
 
-	if (nr_wake < 0 || nr_requeue < 0)
-		return -EINVAL;
-
 	if (requeue_pi) {
 		/*
 		 * Requeue PI only works on two distinct uaddrs. This
