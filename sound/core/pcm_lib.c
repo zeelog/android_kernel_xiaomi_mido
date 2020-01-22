578309191eefce6f911caf431afec9951b6c3ea3
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-01-23 02:12:47 +0700

    Revert "ALSA: pcm: Fix stream lock usage in snd_pcm_period_elapsed()"
    This causes issues with A/V Streaming services, and somehow affected to
    local A/V Playback. Revert this, fix the issue.
    
    This reverts commit a9b98cea99f83e086c5ffe196528be0a8e4ca3f0.

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -1865,14 +1865,11 @@ void snd_pcm_period_elapsed(struct snd_pcm_substream *substream)
 	struct snd_pcm_runtime *runtime;
 	unsigned long flags;
 
-	if (snd_BUG_ON(!substream))
-		return;
-
-	snd_pcm_stream_lock_irqsave(substream, flags);
 	if (PCM_RUNTIME_CHECK(substream))
-		goto _unlock;
+		return;
 	runtime = substream->runtime;
 
+	snd_pcm_stream_lock_irqsave(substream, flags);
 	if (!snd_pcm_running(substream) ||
 	    snd_pcm_update_hw_ptr0(substream, 1) < 0)
 		goto _end;
@@ -1881,7 +1878,6 @@ void snd_pcm_period_elapsed(struct snd_pcm_substream *substream)
 		snd_timer_interrupt(substream->timer, 1);
  _end:
 	kill_fasync(&runtime->fasync, SIGIO, POLL_IN);
- _unlock:
 	snd_pcm_stream_unlock_irqrestore(substream, flags);
 }
 
