ed1bda1fdb51c5ccbb834a80eeeb19a05992a0ba
pengus77 <ivan@cerrato.biz>
2020-02-05 21:49:00 +0200

    wakelocks: block IPA_RM12 by default

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;IPCRTR_lpass_rx;hal_bluetooth_lock"
+#define LIST_WL_DEFAULT			"IPA_RM12;qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;IPCRTR_lpass_rx;hal_bluetooth_lock"
 
 #define LENGTH_LIST_WL			255
 #define LENGTH_LIST_WL_DEFAULT		158
