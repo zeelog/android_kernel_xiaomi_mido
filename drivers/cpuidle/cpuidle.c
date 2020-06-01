886c2b6d210375b0f43085a53593048345cf791c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-06-01 12:13:55 -0700

    cpuidle: Mark CPUs idle as late as possible to avoid unneeded IPIs
    It isn't guaranteed a CPU will idle upon calling lpm_cpuidle_enter(),
    since it could abort early at the need_resched() check. In this case,
    it's possible for an IPI to be sent to this "idle" CPU needlessly, thus
    wasting power. For the same reason, it's also wasteful to keep a CPU
    marked idle even after it's woken up.
    
    Reduce the window that CPUs are marked idle to as small as it can be in
    order to improve power consumption.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -36,6 +36,24 @@ static int enabled_devices;
 static int off __read_mostly;
 static int initialized __read_mostly;
 
+#ifdef CONFIG_SMP
+static atomic_t idled = ATOMIC_INIT(0);
+
+#if NR_CPUS > 32
+#error idled CPU mask not big enough for NR_CPUS
+#endif
+
+void cpuidle_set_idle_cpu(unsigned int cpu)
+{
+	atomic_or(BIT(cpu), &idled);
+}
+
+void cpuidle_clear_idle_cpu(unsigned int cpu)
+{
+	atomic_andnot(BIT(cpu), &idled);
+}
+#endif
+
 int cpuidle_disabled(void)
 {
 	return off;
