2af8bf46f999535593fbf8e3d81877987895562f
Albert I <kras@raphielgang.org>
2020-07-06 12:47:55 +0800

    prima: logging: Treat persistent fatal firmware event messages as debug
    Also ratelimit amount of this kind of message since the event *can* occur
    every few hundred milliseconds or even less, which is flat annoying as we
    already know something happened in dmesg.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- CORE/SVC/src/logging/wlan_logging_sock_svc.c
+++ CORE/SVC/src/logging/wlan_logging_sock_svc.c
@@ -1962,7 +1962,8 @@ void wlan_process_done_indication(uint8 type, uint32 reason_code)
 				spin_unlock_irqrestore(
 					&gwlan_logging.bug_report_lock,
 					flags);
-				pr_info("%s: Ignoring Fatal event from firmware for reason %d\n",
+				pr_debug_ratelimited(
+					"%s: Ignoring Fatal event from firmware for reason %d\n",
 					__func__, reason_code);
 				return;
 			}
