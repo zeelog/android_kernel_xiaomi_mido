LineageOS/android_kernel_xiaomi_msm8953

ASoC: msm8952: Re-enable external speaker pinctrl calls for tissot
These are working properly now, so we can bring them back.

techpack/audio/asoc/msm8952.c
@@ -354,7 +354,7 @@ static int enable_spk_ext_pa(struct snd_soc_codec *codec, int enable)
 {
 	struct snd_soc_card *card = codec->component.card;
 	struct msm_asoc_mach_data *pdata = snd_soc_card_get_drvdata(card);
-#if !((defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TISSOT))
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 	int ret;
 #endif
 #ifdef CONFIG_MACH_XIAOMI_MIDO
@@ -384,7 +384,6 @@ static int enable_spk_ext_pa(struct snd_soc_codec *codec, int enable)
 			pa_mode--;
 		}
 #else
-#ifndef CONFIG_MACH_XIAOMI_TISSOT
 		ret =  msm_cdc_pinctrl_select_active_state(
 					pdata->spk_ext_pa_gpio_p);
 		if (ret) {
@@ -392,12 +391,11 @@ static int enable_spk_ext_pa(struct snd_soc_codec *codec, int enable)
 					__func__, "ext_spk_gpio");
 			return ret;
 		}
-#endif
 		gpio_set_value_cansleep(pdata->spk_ext_pa_gpio, enable);
 #endif
 	} else {
 		gpio_set_value_cansleep(pdata->spk_ext_pa_gpio, enable);
-#if !((defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TISSOT))
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 		ret = msm_cdc_pinctrl_select_sleep_state(
 				pdata->spk_ext_pa_gpio_p);
 		if (ret) {

zeelog/android_kernel_xiaomi_mido

Change-Id: I71a740ba67c0d070cbaa6742f34b60fe69819eb3
Signed-off-by: Albert I <kras@raphielgang.org>

techpack/audio/asoc/msm8952.c
@@ -355,11 +355,10 @@ static int enable_spk_ext_pa(struct snd_soc_codec *codec, int enable)
 {
 	struct snd_soc_card *card = codec->component.card;
 	struct msm_asoc_mach_data *pdata = snd_soc_card_get_drvdata(card);
-#if !((defined CONFIG_MACH_XIAOMI_C6) || (defined CONFIG_MACH_XIAOMI_D2))
-	int ret;
-#endif
 #ifdef CONFIG_MACH_XIAOMI_C6
 	int pa_mode = EXT_PA_MODE;
+#else
+	int ret;
 #endif
 
 	if (!gpio_is_valid(pdata->spk_ext_pa_gpio)) {
@@ -385,7 +384,6 @@ static int enable_spk_ext_pa(struct snd_soc_codec *codec, int enable)
 			pa_mode--;
 		}
 #else
-#ifndef CONFIG_MACH_XIAOMI_D2
 		ret =  msm_cdc_pinctrl_select_active_state(
 					pdata->spk_ext_pa_gpio_p);
 		if (ret) {
@@ -393,12 +391,11 @@ static int enable_spk_ext_pa(struct snd_soc_codec *codec, int enable)
 					__func__, "ext_spk_gpio");
 			return ret;
 		}
-#endif
 		gpio_set_value_cansleep(pdata->spk_ext_pa_gpio, enable);
 #endif
 	} else {
 		gpio_set_value_cansleep(pdata->spk_ext_pa_gpio, enable);
-#if !((defined CONFIG_MACH_XIAOMI_C6) || (defined CONFIG_MACH_XIAOMI_D2))
+#ifndef CONFIG_MACH_XIAOMI_C6
 		ret = msm_cdc_pinctrl_select_sleep_state(
 				pdata->spk_ext_pa_gpio_p);
 		if (ret) {