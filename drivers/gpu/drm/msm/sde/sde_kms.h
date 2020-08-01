e067c37c73f0532a3a56797dbc0c666216a0589b
celtare21 <celtare21@gmail.com>
2020-08-01 18:10:57 +0200

    drm/msm/sde: Remove pm_qos usage
    Signed-off-by: celtare21 <celtare21@gmail.com>
    [kras: Backport to msm-4.9, modify for complete removal]
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/gpu/drm/msm/sde/sde_kms.h
+++ drivers/gpu/drm/msm/sde/sde_kms.h
@@ -21,7 +21,6 @@
 
 #include <linux/msm_ion.h>
 #include <linux/pm_domain.h>
-#include <linux/pm_qos.h>
 
 #include "msm_drv.h"
 #include "msm_kms.h"
@@ -229,7 +228,6 @@ struct sde_kms {
 
 	struct msm_gem_address_space *aspace[MSM_SMMU_DOMAIN_MAX];
 	struct sde_power_client *core_client;
-	struct pm_qos_request pm_qos_cpu_req;
 
 	struct ion_client *iclient;
 	struct sde_power_event *power_event;
