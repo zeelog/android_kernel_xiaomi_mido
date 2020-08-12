1784a463723d71138ca07985eaf6181779398af9
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-08-12 22:47:49 +0700

    msm: vidc_3x: Silence debugfs creation failure spam
    We have it on vidc_3x too :p
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- drivers/media/platform/msm/vidc_3x/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc_3x/msm_vidc_debug.c
@@ -238,7 +238,7 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 	snprintf(debugfs_name, MAX_DEBUGFS_NAME, "core%d", core->id);
 	dir = debugfs_create_dir(debugfs_name, parent);
 	if (IS_ERR_OR_NULL(dir)) {
-		dprintk(VIDC_ERR, "Failed to create debugfs for msm_vidc\n");
+		dprintk(VIDC_DBG, "Failed to create debugfs for msm_vidc\n");
 		goto failed_create_file;
 	}
 
@@ -459,7 +459,7 @@ struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
 
 	dir = debugfs_create_dir(debugfs_name, parent);
 	if (IS_ERR_OR_NULL(dir)) {
-		dprintk(VIDC_ERR, "Failed to create debugfs for msm_vidc\n");
+		dprintk(VIDC_DBG, "Failed to create debugfs for msm_vidc\n");
 		goto failed_create_dir;
 	}
 
