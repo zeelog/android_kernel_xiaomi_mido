b0d33f5091432aef053406a5cbbaaa31e21349c5
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-10-11 16:10:35 +0530

    kernel: power: process: suppress process freezing and related logging on suspend and guard some cont. prints with DEBUG_LOG definition

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -21,6 +21,8 @@
 #include <linux/wakeup_reason.h>
 #include <linux/cpuset.h>
 
+// #define DEBUG_LOG
+
 /*
  * Timeout for stopping processes
  */
@@ -113,8 +115,10 @@ static int try_to_freeze_tasks(bool user_only)
 		}
 		read_unlock(&tasklist_lock);
 	} else {
+		#ifdef DEBUG_LOG
 		pr_cont("(elapsed %d.%03d seconds) ", elapsed_msecs / 1000,
 			elapsed_msecs % 1000);
+		#endif
 	}
 
 	return todo ? -EBUSY : 0;
@@ -142,14 +146,19 @@ int freeze_processes(void)
 		atomic_inc(&system_freezing_cnt);
 
 	pm_wakeup_clear();
-	pr_info("Freezing user space processes ... ");
+	pr_debug("Freezing user space processes ... ");
 	pm_freezing = true;
 	error = try_to_freeze_tasks(true);
 	if (!error) {
 		__usermodehelper_set_disable_depth(UMH_DISABLED);
+	#ifdef DEBUG_LOG
 		pr_cont("done.");
+	#endif
+
 	}
+	#ifdef DEBUG_LOG
 	pr_cont("\n");
+	#endif
 	BUG_ON(in_atomic());
 
 	/*
@@ -178,14 +187,16 @@ int freeze_kernel_threads(void)
 {
 	int error;
 
-	pr_info("Freezing remaining freezable tasks ... ");
+	pr_debug("Freezing remaining freezable tasks ... ");
 
 	pm_nosig_freezing = true;
 	error = try_to_freeze_tasks(false);
+	#ifdef DEBUG_LOG
 	if (!error)
 		pr_cont("done.");
 
 	pr_cont("\n");
+	#endif
 	BUG_ON(in_atomic());
 
 	if (error)
@@ -206,7 +217,7 @@ void thaw_processes(void)
 
 	oom_killer_enable();
 
-	pr_info("Restarting tasks ... ");
+	pr_debug("Restarting tasks ... ");
 
 	__usermodehelper_set_disable_depth(UMH_FREEZING);
 	thaw_workqueues();
@@ -227,7 +238,9 @@ void thaw_processes(void)
 	usermodehelper_enable();
 
 	schedule();
+	#ifdef DEBUG_LOG
 	pr_cont("done.\n");
+	#endif
 	trace_suspend_resume(TPS("thaw_processes"), 0, false);
 }
 
@@ -236,7 +249,7 @@ void thaw_kernel_threads(void)
 	struct task_struct *g, *p;
 
 	pm_nosig_freezing = false;
-	pr_info("Restarting kernel threads ... ");
+	pr_debug("Restarting kernel threads ... ");
 
 	thaw_workqueues();
 
@@ -248,5 +261,7 @@ void thaw_kernel_threads(void)
 	read_unlock(&tasklist_lock);
 
 	schedule();
+	#ifdef DEBUG_LOG
 	pr_cont("done.\n");
+	#endif
 }
