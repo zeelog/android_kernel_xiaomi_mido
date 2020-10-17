3c8e92086f0169131676d066f949f67ac0c7aac2
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-10-17 15:43:08 +0530

    base: power: wakeup: ignore last active wakeup logging

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -906,11 +906,13 @@ void pm_get_active_wakeup_sources(char *pending_wakeup_source, size_t max)
 			last_active_ws = ws;
 		}
 	}
+#if 0
 	if (!active && last_active_ws) {
 		scnprintf(pending_wakeup_source, max,
 				"Last active Wakeup Source: %s",
 				last_active_ws->name);
 	}
+#endif
 	srcu_read_unlock(&wakeup_srcu, srcuidx);
 }
 EXPORT_SYMBOL_GPL(pm_get_active_wakeup_sources);
@@ -938,7 +940,7 @@ void pm_print_active_wakeup_sources(void)
 	}
 
 	if (!active && last_activity_ws)
-		pr_info("last active wakeup source: %s\n",
+		pr_debug("last active wakeup source: %s\n",
 			last_activity_ws->name);
 	srcu_read_unlock(&wakeup_srcu, srcuidx);
 }
