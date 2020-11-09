89be98b4e3203c0b74d313c12d8bad606dab4493
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-11-09 09:50:25 +0700

    drivers: base: boeffla: Update wl list
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT			"CHG_PLCY_MAIN_WL;NETLINK;netmgr_wl;[timerfd];event0;event1;eventpoll;KeyEvents;IPA_WS;IPA_RM12;"
+#define LIST_WL_DEFAULT			"CHG_PLCY_MAIN_WL;NETLINK;[timerfd];event0;event1;eventpoll;KeyEvents;IPA_WS;IPA_RM12;ipc_rtr_smd_ipcrtr;ApmAudio;ApmOutput;sensor_ind;netmgr_wl"
 
 #define LENGTH_LIST_WL			512
 #define LENGTH_LIST_WL_DEFAULT		512
