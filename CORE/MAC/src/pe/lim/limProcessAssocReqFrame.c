579ed24ca929e40220cb4abe3ba8ac5a5c549287
Albert I <kras@raphielgang.org>
2020-12-08 18:24:14 +0800

    prima: Fix build without SAE feature
    This commit depends on commit 1341c9778 ("prima: Fix build with LFR Make
    Before Break feature disabled") as it contains half of the required fix.
    
    This just addresses remaining build errors.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c
+++ CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c
@@ -257,12 +257,6 @@ static enum eSirMacStatusCodes lim_check_sae_pmf_cap(tpPESession session,
 
     return status;
 }
-#else
-static enum eSirMacStatusCodes lim_check_sae_pmf_cap(tpPESession session,
-                                                    tDot11fIERSN *rsn)
-{
-    return eSIR_MAC_SUCCESS_STATUS;
-}
 #endif
 
 bool lim_send_assoc_ind_to_sme(tpAniSirGlobal pMac,
@@ -1550,6 +1544,7 @@ limProcessAssocReqFrame(tpAniSirGlobal pMac, tANI_U8 *pRxPacketInfo,
                 akm_type = lim_translate_rsn_oui_to_akm_type(
                                     Dot11fIERSN.akm_suite[0]);
 
+#ifdef WLAN_FEATURE_SAE
                 if (akm_type == ANI_AKM_TYPE_SAE) {
                     if (eSIR_SUCCESS != (status =
                         lim_check_sae_pmf_cap(psessionEntry, &Dot11fIERSN))) {
@@ -1565,6 +1560,7 @@ limProcessAssocReqFrame(tpAniSirGlobal pMac, tANI_U8 *pRxPacketInfo,
                         goto error;
                     }
                 }
+#endif
 
             } /* end - if(pAssocReq->rsnPresent) */
             if((!pAssocReq->rsnPresent) && pAssocReq->wpaPresent)
