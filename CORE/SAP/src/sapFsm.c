caf-backup/quic_la_platform_vendor_qcom-opensource_wlan_qcacld-2.0

qcacld-2.0: fix reassociation issue
While getting reassociation request frame, user space hostapd will
trigger diassociate with reason code 13. Association can't be
established.

The root cause is host driver forwards more 6 bytes to user space.
The 6 bytes is current AP address in association request frame.
Hostapd can't paras association request frame because the additional
6 bytes.

Skip current AP address before forwarding reassociation request
frame to user space.

Change-Id: I94a7c26289f24a1f0716fe64c4ee58eae1a913c6
CRs-Fixed: 2717695

CORE/SAP/src/sapFsm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2020 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -3680,6 +3680,11 @@ sapSignalHDDevent
                                      ASSOC_REQ_IE_OFFSET);
             sta_event_ptr->ies = (pCsrRoamInfo->assocReqPtr +
                                      ASSOC_REQ_IE_OFFSET);
+            if (pCsrRoamInfo->fReassocReq) {
+                sta_event_ptr->iesLen -= VOS_MAC_ADDR_SIZE;
+                sta_event_ptr->ies += VOS_MAC_ADDR_SIZE;
+            }
+
             /* also fill up the channel info from the csrRoamInfo */
             pChanInfo =
             &sta_event_ptr->chan_info;

zeelog/android_kernel_xiaomi_mido

CORE/SAP/src/sapFsm.c
@@ -861,6 +861,11 @@ sapSignalHDDevent
             event->HTCaps = pCsrRoamInfo->ht_caps;
             event->VHTCaps = pCsrRoamInfo->vht_caps;
 
+            if (pCsrRoamInfo->fReassocReq) {
+                event->iesLen -= VOS_MAC_ADDR_SIZE;
+                event->ies += VOS_MAC_ADDR_SIZE;
+            }
+
             //TODO: Need to fill sapAuthType
             //event->SapAuthType = pCsrRoamInfo->pProfile->negotiatedAuthType;
             break;