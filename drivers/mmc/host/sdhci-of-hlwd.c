79aa704560615d9ea6f848c148a797cc6081572b
Dicky Herlambang <herlambangdicky5@gmail.com>
2021-01-06 07:28:43 +0700

    SQUASH: Revert Nuke PM when suspend skip sync enable
    This reverts commit 05ce15fd8e145f94761928f2f1a293a7e416d3d8.
    This reverts commit 0a42a20e729bffe87c822f811ef1a956339760e9.

--- drivers/mmc/host/sdhci-of-hlwd.c
+++ drivers/mmc/host/sdhci-of-hlwd.c
@@ -85,9 +85,7 @@ static struct platform_driver sdhci_hlwd_driver = {
 	.driver = {
 		.name = "sdhci-hlwd",
 		.of_match_table = sdhci_hlwd_of_match,
-	#ifndef CONFIG_SUSPEND_SKIP_SYNC
 		.pm = &sdhci_pltfm_pmops,
-	#endif
 	},
 	.probe = sdhci_hlwd_probe,
 	.remove = sdhci_pltfm_unregister,
