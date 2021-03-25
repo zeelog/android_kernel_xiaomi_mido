2018c0f73bf0bf1171ad687f6d14dd78cb2b7af2
Dicky Herlambang <herlambangdicky5@gmail.com>
2021-03-25 09:00:34 +0700

    Merge branch 'LA.UM.9.6.2' of https://github.com/HANA-CI-Build-Project/vendor_qcom_opensource_wlan_prima into lineage-17.1
    * 'LA.UM.9.6.2' of https://github.com/HANA-CI-Build-Project/vendor_qcom_opensource_wlan_prima:
      prima: Send assoc reject upon failing to post ASSOC_IND
      wlan: Decode length field in the ANI msg header
    
    Upstreamed prima WLAN tag to LA.UM.9.6.2.r1-04200-89xx.0
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c
@@ -1673,7 +1673,7 @@ error:
 \param  pMac
 \param  *pStaDs - Station DPH hash entry
 \param  psessionEntry - PE session entry
-\return None
+\return tSirRetStatus
 
  * ?????? How do I get 
  *  - subtype   =====> psessionEntry->parsedAssocReq.reassocRequest
@@ -1683,7 +1683,7 @@ error:
  *  - pHdr->seqControl  =====> no longer needed
  *  - pStaDs
 ------------------------------------------------------------------*/
-void limSendMlmAssocInd(tpAniSirGlobal pMac, tpDphHashNode pStaDs, tpPESession psessionEntry)
+tSirRetStatus limSendMlmAssocInd(tpAniSirGlobal pMac, tpDphHashNode pStaDs, tpPESession psessionEntry)
 {
     tpLimMlmAssocInd        pMlmAssocInd = NULL;
     tpLimMlmReassocInd      pMlmReassocInd;
@@ -1722,7 +1722,7 @@ void limSendMlmAssocInd(tpAniSirGlobal pMac, tpDphHashNode pStaDs, tpPESession p
         {
             limReleasePeerIdx(pMac, pStaDs->assocId, psessionEntry);
             limLog(pMac, LOGP, FL("AllocateMemory failed for pMlmAssocInd"));
-            return;
+            return eSIR_MEM_ALLOC_FAILED;
         }
         vos_mem_set(pMlmAssocInd, temp ,0);
 
@@ -1776,7 +1776,7 @@ void limSendMlmAssocInd(tpAniSirGlobal pMac, tpDphHashNode pStaDs, tpPESession p
                 PELOGE(limLog(pMac, LOGE, FL("rsnIEdata index out of bounds %d"),
                                               pMlmAssocInd->rsnIE.length);)
                 vos_mem_free(pMlmAssocInd);
-                return;
+                return eSIR_FAILURE;
             }
             pMlmAssocInd->rsnIE.rsnIEdata[pMlmAssocInd->rsnIE.length] = SIR_MAC_WPA_EID;
             pMlmAssocInd->rsnIE.rsnIEdata[pMlmAssocInd->rsnIE.length + 1] = pAssocReq->wpa.length;
@@ -1896,7 +1896,7 @@ void limSendMlmAssocInd(tpAniSirGlobal pMac, tpDphHashNode pStaDs, tpPESession p
             limLog(pMac, LOGP, FL("call to AllocateMemory failed for "
                                   "pMlmReassocInd"));
             limReleasePeerIdx(pMac, pStaDs->assocId, psessionEntry);
-            return;
+            return eSIR_MEM_ALLOC_FAILED;
         }
         vos_mem_set(pMlmReassocInd, temp, 0);
 
@@ -2020,6 +2020,6 @@ void limSendMlmAssocInd(tpAniSirGlobal pMac, tpDphHashNode pStaDs, tpPESession p
         vos_mem_free(pMlmReassocInd);
     }
 
-    return;
+    return eSIR_SUCCESS;
 
 } /*** end limSendMlmAssocInd() ***/
