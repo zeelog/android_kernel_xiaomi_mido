d309728b300058a393ec952b2ea8ba26992f0d8b
Gaurav Jindal <quic_gjindal@quicinc.com>
2022-06-27 13:42:27 +0530

    msm: camera: Add Page Read, DelayUSEC support in camera i2c
    Add Page Read, writeDelayUSEC and Continuous Read support in
    camera i2c operations.
    
    Change-Id: I58e269477c8158dd1cc394925cb7a3d175546b0a
    Signed-off-by: Gaurav Jindal <quic_gjindal@quicinc.com>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -258,6 +258,9 @@ enum msm_camera_i2c_operation {
 #define MSM_CAM_READ_LOOP \
 	MSM_CAM_READ_LOOP
 	MSM_CAM_READ_LOOP = 3,
+	MSM_CAM_READ_PAGE,
+	MSM_CAM_WRITE_DELAYUSEC,
+	MSM_CAM_READ_CONTINUOUS,
 };
 
 struct msm_sensor_i2c_sync_params {
