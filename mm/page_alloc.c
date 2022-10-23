66a2c8b373150744b68f8ba6c27c981c43bf3348
Sultan Alsawaf <sultan@kerneltoast.com>
2022-10-23 21:49:22 +0200

    mm: Boost when memory pressure becomes high
    When memory pressure starts to balloon, allocation latency takes a hit
    as the page allocator continuously tries to find a way to free up some
    pages. Boosting when this occurs can significantly help reduce
    allocation latency, preventing noticeable lag from being observed.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -68,6 +68,7 @@
 #include <linux/show_mem_notifier.h>
 #include <linux/khugepaged.h>
 #include <linux/psi.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -3737,6 +3738,9 @@ retry_cpuset:
 		}
 		wake_all_kswapds(order, ac);
 	}
+
+       /* Boost DDR bus when memory is low so allocation latency doesn't get too bad */
+	devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 
 	/*
 	 * The adjusted alloc_flags might result in immediate success, so try
