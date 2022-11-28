30a47207170898bb0aa8350f8c93944b75e93b9d
Pranav Vashi <neobuddy89@gmail.com>
2022-11-28 23:48:11 +0530

    techpack: audio: Fix enum conversion clang error
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- techpack/audio/asoc/codecs/wcd_cpe_services.c
+++ techpack/audio/asoc/codecs/wcd_cpe_services.c
@@ -655,7 +655,7 @@ static void cpe_notify_cmi_client(struct cpe_info *t_info, u8 *payload,
 	service = CMI_HDR_GET_SERVICE(hdr);
 
 	notif.event = CMI_API_MSG;
-	notif.result = result;
+	notif.result = (enum cmi_api_result)result;
 	notif.message = payload;
 
 	CPE_SVC_GRAB_LOCK(&cpe_d.cpe_svc_lock, "cpe_svc");
