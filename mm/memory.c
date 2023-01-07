b4a7f66c097ad0de172bd3279ae0f7c0a66c1a75
zeelog <Danjel.Zammit@gmail.com>
2023-01-07 18:17:10 +0100

    Revert "mm: account pud page tables"
    This reverts commit 6a67b5ed3bbeb596f13d49b1da595d1201324de4.

--- mm/memory.c
+++ mm/memory.c
@@ -485,7 +485,6 @@ static inline void free_pud_range(struct mmu_gather *tlb, pgd_t *pgd,
 	pud = pud_offset(pgd, start);
 	pgd_clear(pgd);
 	pud_free_tlb(tlb, pud, start);
-	mm_dec_nr_puds(tlb->mm);
 }
 
 /*
@@ -4256,13 +4255,10 @@ int __pud_alloc(struct mm_struct *mm, pgd_t *pgd, unsigned long address)
 	smp_wmb(); /* See comment in __pte_alloc */
 
 	spin_lock(&mm->page_table_lock);
-	if (pgd_present(*pgd)) {
-		/* Another has populated it */
+	if (pgd_present(*pgd))		/* Another has populated it */
 		pud_free(mm, new);
-	} else {
-		mm_inc_nr_puds(mm);
+	else
 		pgd_populate(mm, pgd, new);
-	}
 	spin_unlock(&mm->page_table_lock);
 	return 0;
 }
