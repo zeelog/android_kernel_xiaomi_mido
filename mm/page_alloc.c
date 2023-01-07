4be8e14642a9b05575b265b1e457e018d099a06b
zeelog <Danjel.Zammit@gmail.com>
2023-01-07 18:10:41 +0100

    Revert "mm: oom: print zspages and ion_heap, ion_heap pool"
    This reverts commit cd795c81fa28f1d7d6d0872189addfc80cd210a9.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4481,8 +4481,7 @@ void show_free_areas(unsigned int filter)
 		" unevictable:%lu dirty:%lu writeback:%lu unstable:%lu\n"
 		" slab_reclaimable:%lu slab_unreclaimable:%lu\n"
 		" mapped:%lu shmem:%lu pagetables:%lu bounce:%lu\n"
-		" free:%lu free_pcp:%lu free_cma:%lu zspages: %lu\n"
-		" ion_heap:%lu ion_heap_pool:%lu\n",
+		" free:%lu free_pcp:%lu free_cma:%lu\n",
 		global_node_page_state(NR_ACTIVE_ANON),
 		global_node_page_state(NR_INACTIVE_ANON),
 		global_node_page_state(NR_ISOLATED_ANON),
@@ -4501,15 +4500,7 @@ void show_free_areas(unsigned int filter)
 		global_page_state(NR_BOUNCE),
 		global_page_state(NR_FREE_PAGES),
 		free_pcp,
-		global_page_state(NR_FREE_CMA_PAGES),
-#if IS_ENABLED(CONFIG_ZSMALLOC)
-		global_page_state(NR_ZSPAGES),
-#else
-		0UL,
-#endif
-		global_node_page_state(NR_ION_HEAP),
-		global_node_page_state(NR_INDIRECTLY_RECLAIMABLE_BYTES)
-							>> PAGE_SHIFT);
+		global_page_state(NR_FREE_CMA_PAGES));
 
 	for_each_online_pgdat(pgdat) {
 		printk("Node %d"
