0d93ae9d7ac68327389e9f7a82e24d5f83666a6f
zeelog <Danjel.Zammit@gmail.com>
2023-01-07 18:09:28 +0100

    Revert "mm, page_alloc: double zone's batchsize"
    This reverts commit 97b898566c347800c790f37d473b42996bb571a2.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5321,12 +5321,13 @@ static int zone_batchsize(struct zone *zone)
 
 	/*
 	 * The per-cpu-pages pools are set to around 1000th of the
-	 * size of the zone.
+	 * size of the zone.  But no more than 1/2 of a meg.
+	 *
+	 * OK, so we don't know how big the cache is.  So guess.
 	 */
 	batch = zone->managed_pages / 1024;
-	/* But no more than a meg. */
-	if (batch * PAGE_SIZE > 1024 * 1024)
-		batch = (1024 * 1024) / PAGE_SIZE;
+	if (batch * PAGE_SIZE > 512 * 1024)
+		batch = (512 * 1024) / PAGE_SIZE;
 	batch /= 4;		/* We effectively *= 4 below */
 	if (batch < 1)
 		batch = 1;
