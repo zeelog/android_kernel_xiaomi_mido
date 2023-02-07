a66cffd4cf24ef71c361bfd100b814e6f8494fcc
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-02-07 23:56:16 +0600

    Partially revert "lpm-levels: Allow CPU to idle when executing audio code"
    This doesn't work as intended.
    But keep msm_cpuidle_get_deep_idle_latency() inlined as it's correct.
    
    This partially reverts commit d0718deef8f975bfd42212cdee412a226d8516d7.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: Ie543c81d545620a73d44af2c686381bd89402adf
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -92,7 +92,7 @@ module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
  */
 inline s32 msm_cpuidle_get_deep_idle_latency(void)
 {
-	return 100;
+	return 10;
 }
 EXPORT_SYMBOL(msm_cpuidle_get_deep_idle_latency);
 
