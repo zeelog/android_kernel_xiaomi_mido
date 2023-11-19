WeeAris/Realking_su_xiaomi_sm8250

FROMLIST: md: workqueue: Remove WQ_CPU_INTENSIVE from unbound workqueue allocations
WQ_CPU_INTENSIVE is meaningless for the unbound workqueue, so remove it.

Signed-off-by: Xunlei Pang <pang.xunlei@linaro.org>
Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

drivers/md/dm-verity-target.c
@@ -1185,7 +1185,7 @@ int verity_ctr(struct dm_target *ti, unsigned argc, char **argv)
 	}
 
 	/* WQ_UNBOUND greatly improves performance when running on ramdisk */
-	v->verify_wq = alloc_workqueue("kverityd", WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM | WQ_UNBOUND, num_online_cpus());
+	v->verify_wq = alloc_workqueue("kverityd", WQ_MEM_RECLAIM | WQ_UNBOUND, num_online_cpus());
 	if (!v->verify_wq) {
 		ti->error = "Cannot allocate workqueue";
 		r = -ENOMEM;

zeelog/android_kernel_xiaomi_mido

Change-Id: If5e278a5f308a71bcb898fdd0d1ce9e19b101586
Signed-off-by: TogoFire <togofire@mailfence.com>

drivers/md/dm-verity-target.c
@@ -1275,7 +1275,7 @@ retry_dev2:
 	 * will fall-back to using it for error handling (or if the bufio cache
 	 * doesn't have required hashes).
 	 */
-	v->verify_wq = alloc_workqueue("kverityd", WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM | WQ_HIGHPRI, num_online_cpus());
+	v->verify_wq = alloc_workqueue("kverityd", WQ_MEM_RECLAIM | WQ_HIGHPRI, num_online_cpus());
 	if (!v->verify_wq) {
 		ti->error = "Cannot allocate workqueue";
 		r = -ENOMEM;